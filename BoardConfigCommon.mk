d4520cb007c01ca717caf3e3b09dacb891695554
Davis Mosenkovs <davikovs@gmail.com>
2018-01-31 14:56:19 -0800

    Fix adaptive-playback for video decoders
    Change-Id: If8426a35ad123b6d9e8feb6679cc08e133573cdd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,6 +34,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 
+# Media
+TARGET_OMX_LEGACY_RESCALING := true
+
 # Binder
 TARGET_USES_64_BIT_BINDER := true
 
