59db590aa87be5d5b99cd49df036093fea98f964
francitato <32774517+francitato@users.noreply.github.com>
2018-03-18 08:02:28 +0000

    zero: enable AdvancedDisplay
    crdroid nougat support it, then is better enable it

--- zero-common.mk
+++ zero-common.mk
@@ -144,8 +144,8 @@ PRODUCT_PACKAGES += \
     libstlport
 
 # MDNIE
-#PRODUCT_PACKAGES += \
-#    AdvancedDisplay
+PRODUCT_PACKAGES += \
+    AdvancedDisplay
 
 # Media profile
 PRODUCT_COPY_FILES += \
